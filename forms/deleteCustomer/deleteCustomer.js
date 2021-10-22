644142f319196d4dee36482858191568b5162d11
Michael-Petrakis <90578378+Michael-Petrakis@users.noreply.github.com>
2021-10-21 20:29:57 -0500

    Version 2

---
+++ forms/deleteCustomer/deleteCustomer.js
@@ -0,0 +1,33 @@
+
+  
+ deleteButton.onclick=function(){
+  let query = "SELECT * FROM customer"
+  req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
+  results = JSON.parse(req.responseText)
+  let message = results[0] + "<br />" + results[1] + "<br />" + results[2] + "<br />" + results[3] + "<br />" + results[4] 
+  deleteLabel.innerHTML = message
+} 
+  
+  /*
+  deleteCustomer = inptCustomer.value
+  deleteCustomer = deleteCustomer.toLowerCase()
+  if (deleteCustomer == 
+  
+  message = message = employees[i] + " " + employees[i+1] + "<br />"
+  lblOutput.innerHTML = message
+  
+Button1.onclick=function(){
+  
+}
+*/
+buttonToDelete.onclick=function(){
+    let found = false
+    let customerDelete = inptDelete.value
+    let query = "SELECT * FROM customer"   
+    req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
+    results = JSON.parse(req.responseText)
+    for (i = 0; i <= results.length - 1; i++) {
+       deleteCustomerLabel = (` results[i] is ${results[i]}`)
+       if (customerDelete == results[i])
+            found = true
+}
