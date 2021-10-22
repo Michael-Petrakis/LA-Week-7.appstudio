7b74ec33582f61cda7baf409fee4829dedb7e832
Michael-Petrakis <90578378+Michael-Petrakis@users.noreply.github.com>
2021-10-21 20:45:33 -0500

    Final Version

--- forms/selectCustomer/selectCustomer.js
+++ forms/selectCustomer/selectCustomer.js
@@ -6,11 +6,11 @@ let pw = "Password"
 
 
 selectButton.onclick=function(){
-  let query = "SELECT name FROM customer"
+  let query = "SELECT * FROM customer"
   req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
   results = JSON.parse(req.responseText)
-  selectLabel.textContent = results
-  
-
-
+  message = ""
+  for (i = 0; i < results.length; i++)
+    message = message + "<br />" + results[i][1]
+  selectLabel.innerHTML = message
 }
